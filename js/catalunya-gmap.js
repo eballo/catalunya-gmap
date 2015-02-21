7e47806a9daa50d9519d54fdf2772b4bad38788f
Enric Ballo <enric.ballo@gmail.com>
2015-02-21 11:49:05 +0100

    add MAP_OPTIONS

--- js/catalunya-gmap.js
+++ js/catalunya-gmap.js
@@ -1,30 +1,12 @@
-(function(window, google) {
+(function(window, google, mapster) {
 
-	//Position of Catalunya 
-	var latitud = 41.440908754848165;
-	var longitude = 1.81713925781257;
-    var catalunya = new google.maps.LatLng(latitud, longitude);
+	//load the configuration of the map
+	var options = mapster.MAP_OPTIONS;
 
-	//maps options
-	var options = {
-		center: catalunya,
-		zoom: 8,
-		streetViewControl: false,
-	      mapTypeControl: true,
-	      mapTypeControlOptions: {
-	        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-	        position: google.maps.ControlPosition.TOP_LEFT
-	      },
-	      zoomControl: true,
-	      zoomControlOptions: {
-	        style: google.maps.ZoomControlStyle.NORMAL
-	      },
-	      mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-
+	//get the gMap element
 	var element =  document.getElementById('gMap');
 
 	//create the map
 	var map = new google.maps.Map(element, options);
 	
-}(window, google));
+}(window, google, window.Mapster || (window.Mapster = {}) ));
---
+++ js/catalunya-options-gmap.js
@@ -0,0 +1,43 @@
+/**
+ * Catalunya options for GoogleMaps
+ * 
+ */
+(function(window, google, mapster) {
+
+//Position of Catalunya 
+var latitud = 41.440908754848165;
+var longitude = 1.81713925781257;
+var catalunya = new google.maps.LatLng(latitud, longitude);
+
+//maps options
+mapster.MAP_OPTIONS = {
+	center: catalunya,
+	zoom: 7,
+	maxZoom: 10,
+	minZoom: 4,
+	streetViewControl: false,
+	disableDefaultUI: false,
+	scrollwheel: true,
+	draggable: true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        position: google.maps.ControlPosition.TOP_LEFT
+    },
+    panControl: true,
+    panControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.NORMAL
+    },
+    scaleControl: true,  // fixed to BOTTOM_RIGHT
+    streetViewControl: true,
+    streetViewControlOptions: {
+        position: google.maps.ControlPosition.LEFT_TOP
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+}(window, google, window.Mapster || (window.Mapster = {}) ));
