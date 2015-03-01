ed07fad9d467ee98345cf791d2dd0b0b22e26b37
Enric Ballo <enric.ballo@gmail.com>
2015-03-01 22:00:04 +0100

    cleaning code

--- js/catalunya-gmap.js
+++ js/catalunya-gmap.js
@@ -13,7 +13,6 @@
 		id:1,
 		lat: 42.307682, 
 		lng: 3.011110,
-		drabagle: false,
 		visible: true,
 		content: 'Castell 1',
 		icon: 'http://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/gmap/militar/castell/castell7.png'
@@ -23,7 +22,6 @@
 		id:2,
 		lat: 42.000682, 
 		lng: 3.071110,
-		drabagle: false,
 		visible: true,
 		content: 'Castell 2',
 		icon: 'http://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/gmap/militar/castell/castell7.png'
@@ -33,7 +31,6 @@
 		id:3,
 		lat: 42.020682, 
 		lng: 3.000110,
-		drabagle: false,
 		visible: true,
 		content: 'Torre 1',
 		icon: 'http://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/gmap/militar/torre/torre7.png'
