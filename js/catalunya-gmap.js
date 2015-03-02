6df2bd313960b8fbfd2ee4939dd0e30d78290de2
Enric Ballo <enric.ballo@gmail.com>
2015-03-02 20:28:28 +0100

    Add testing castles

--- js/catalunya-gmap.js
+++ js/catalunya-gmap.js
@@ -9,32 +9,19 @@
 	//create the map
 	var map = catmap.create(element, options);
 
-	map.addMarker({
-		id:1,
-		lat: 42.307682, 
-		lng: 3.011110,
-		visible: true,
-		content: 'Castell 1',
-		icon: 'http://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/gmap/militar/castell/castell7.png'
-	});
-
-	map.addMarker({
-		id:2,
-		lat: 42.000682, 
-		lng: 3.071110,
-		visible: true,
-		content: 'Castell 2',
-		icon: 'http://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/gmap/militar/castell/castell7.png'
-	});
-
-	map.addMarker({
-		id:3,
-		lat: 42.020682, 
-		lng: 3.000110,
-		visible: true,
-		content: 'Torre 1',
-		icon: 'http://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/gmap/militar/torre/torre7.png'
-	});
+	console.log(aCastells);
+	
+	for(var x =0; x < aCastells.length; x ++){
+		
+		map.addMarker({
+			id: x,
+			lat: Number(aCastells[x].position.lat), 
+			lng: Number(aCastells[x].position.lng),
+			visible: true,
+			content: aCastells[x].position.title,
+			icon: 'http://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/gmap/militar/castell/castell7.png'
+		});
+	}
 
 	console.log(map.markers);
 	
