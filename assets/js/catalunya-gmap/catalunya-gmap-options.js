3bbc764878c0ffea761857e92c8667f977c0d7a9
eballo <enric.ballo@gmail.com>
2017-08-22 20:05:27 +0200

    after release

--- assets/js/catalunya-gmap/catalunya-gmap-options.js
+++ assets/js/catalunya-gmap/catalunya-gmap-options.js
@@ -2,13 +2,13 @@
  * Catalunya options for GoogleMaps
  *
  */
-MarkerClusterer.IMAGE_PATH = 'https://gmap.catalunyamedieval.dev/assets/images/catalunya-gmap/gmap/m';
+MarkerClusterer.IMAGE_PATH = 'https://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/images/catalunya-gmap/gmap/m';
 (function(window, google, gmap) {
 
     gmap.CONFIG_OPTIONS = {
-        serverHost: 'https://gmap.catalunyamedieval.dev/',
+        serverHost: 'https://www.catalunyamedieval.es/wp-content/themes/catalunyamedieval/',
         styleType1: 7,
-        styleType2: 6,
+        styleType2: 8,
         debug: false,
         useMarkerCluster: true
     }
@@ -17,82 +17,6 @@ MarkerClusterer.IMAGE_PATH = 'https://gmap.catalunyamedieval.dev/assets/images/c
     var latitud = 41.440908754848165;
     var longitude = 1.81713925781257;
     var catalunya = new google.maps.LatLng(latitud, longitude);
-    var styles = [{
-        "featureType": "landscape",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "lightness": 65
-        }, {
-            "visibility": "on"
-        }]
-    }, {
-        "featureType": "poi",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "lightness": 51
-        }, {
-            "visibility": "simplified"
-        }]
-    }, {
-        "featureType": "road.highway",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "visibility": "simplified"
-        }]
-    }, {
-        "featureType": "road.arterial",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "lightness": 30
-        }, {
-            "visibility": "on"
-        }]
-    }, {
-        "featureType": "road.local",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "lightness": 40
-        }, {
-            "visibility": "on"
-        }]
-    }, {
-        "featureType": "transit",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "visibility": "simplified"
-        }]
-    }, {
-        "featureType": "administrative.province",
-        "stylers": [{
-            "visibility": "off"
-        }]
-    }, {
-        "featureType": "water",
-        "elementType": "labels",
-        "stylers": [{
-            "visibility": "on"
-        }, {
-            "lightness": -25
-        }, {
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [{
-            "hue": "#ffff00"
-        }, {
-            "lightness": -25
-        }, {
-            "saturation": -97
-        }]
-    }];
 
     //maps options
     gmap.MAP_OPTIONS = {
