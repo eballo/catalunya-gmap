76455d57466d2616c9aa8e9a1cd85ca9157d3728
eballo <enric.ballo@gmail.com>
2017-10-27 11:25:33 +0200

    add remove accents

--- src/js/catalunya-gmap-extra.js
+++ src/js/catalunya-gmap-extra.js
@@ -1,3 +1,21 @@
+
+function removeAccents(p) {
+  value = p.replace("(","");
+  value = value.replace(")","");
+
+  c = 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ';
+  s = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
+  n = '';
+  for (i = 0; i < value.length; i++) {
+    if (c.search(value.substr(i, 1)) >= 0) {
+      n += s.substr(c.search(value.substr(i, 1)), 1);
+    } else {
+      n += value.substr(i, 1);
+    }
+  }
+  return n;
+}
+
 /**
  * Search function
  */
@@ -5,22 +23,22 @@ function searchLlista() {
     // Declare variables
     var input, filter, ul, li, value, i;
     input = document.getElementById('search-llista');
-    filter = input.value.toUpperCase();
+    filter = removeAccents(input.value).toUpperCase();
     ul = document.getElementById("mapLlist");
     li = ul.getElementsByTagName('li');
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-        value = li[i].innerHTML
+        value = removeAccents(li[i].innerHTML);
 
-        if (value != '') {
-            if (value.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
-            }
-        } else {
-            li[i].style.display = "";
+        if(value!=''){
+          if (value.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "";
+          } else {
+              li[i].style.display = "none";
+          }
+        }else{
+          li[i].style.display = "";
         }
     }
 }
